7bb55143809af58b299dfc1a32675587767f857e
Ismael Machado <ismael.machado@globant.com>
2019-12-03 00:01:23 -0300

    Territories map updated

--- assets/territorios/territory.js
+++ assets/territorios/territory.js
@@ -277,7 +277,7 @@ function findTerritory(geoXmlDoc, territory) {
 function focusTerritory(geoXmlDoc, map, options) {
 	var territory = options.params.territorio;
 	if (territory !== undefined) {
-		var placemark = findTerritory(geoXmlDoc);
+		var placemark = findTerritory(geoXmlDoc, territory);
 		var bounds = new google.maps.LatLngBounds();
 		var coordinates = placemark.Polygon[0].outerBoundaryIs[0].coordinates;
 		$.each(coordinates, function(index, coordinate) {
